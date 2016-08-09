adc4e6b31201915a42c71f7197456e8ac335cf35
Chris McClimans <chris@hippiehacker.org>
2016-08-10 11:15:50 +1200

    update errors in mgmt_framework.ps1

--- script/mgmt_framework.ps1
+++ script/mgmt_framework.ps1
@@ -1,19 +1,26 @@
-if ($PSVersionTable.PSVersion.Major -le 4)
+if( $PSVersionTable.PSVersion.Major -le 4 -and -Not  (Get-Hotfix KB3134758))
 {
     if( -Not $env:MGMT_FRAMEWORK5_URL){
         $env:MGMT_FRAMEWORK5_URL = "https://download.microsoft.com/download/2/C/6/2C6E1B4A-EBE5-48A6-B225-2D2058A9CEFB/Win8.1AndW2K12R2-KB3134758-x64.msu" }
-
     if( -Not $env:MGMT_FRAMEWORK5_FILE){
-        $env:MGMT_FRAMEWORK5_FILE = "C:\" + [io.path]::GetFileNameWithoutExtension(
-            $env:MGMT_FRAMEWORK5_URL)
+        $env:MGMT_FRAMEWORK5_FILE = "C:\" + [io.path]::GetFileName($env:MGMT_FRAMEWORK5_URL)
     }
-
-    if( -Not $env:MGMT_FRAMEWORK5_SHA){
-        $env:MGMT_FRAMEWORK5_SHA = "7e8778610bfe23d9eea8bea1d396a399455d7bda"
+    if( -Not $env:MGMT_FRAMEWORK5_CHECKSUM_TYPE){
+        $env:MGMT_FRAMEWORK5_CHECKSUM_TYPE = "SHA1"
     }
-
-    invoke-webrequest $env:MGMT_FRAMWORK5_URL -outfile $env:MGMT_FRAMWORK5_FILE
-
-    start-process wusa -ArgumentList $env:MGMT_FRAMWORK5_FILE, "/quiet", "/norestart" -wait
-    # if(test-path $env:MGMT_FRAMWORK5_FILE){remove-item $env:MGMT_FRAMWORK5_FILE}
+    if( -Not $env:MGMT_FRAMEWORK5_CHECKSUM){
+        $env:MGMT_FRAMEWORK5_CHECKSUM = "7e8778610bfe23d9eea8bea1d396a399455d7bda"
+    }
+    if( -Not (test-path $env:MGMT_FRAMEWORK5_FILE)){
+        invoke-webrequest $env:MGMT_FRAMEWORK5_URL -outfile $env:MGMT_FRAMEWORK5_FILE
+    }
+    $checksum = ( Get-FileHash -Algorithm $env:MGMT_FRAMEWORK5_CHECKSUM_TYPE $env:MGMT_FRAMEWORK5_FILE ).hash
+    if ($checksum -eq $env:MGMT_FRAMEWORK5_CHECKSUM) {
+        start-process wusa -ArgumentList $env:MGMT_FRAMEWORK5_FILE, "/quiet", "/norestart" -wait
+    }
+    else {
+        Write-Host "Checksum doesn't match!"
+        exit 1
+    }
+    if(test-path $env:MGMT_FRAMEWORK5_FILE){remove-item $env:MGMT_FRAMEWORK5_FILE}
 }
